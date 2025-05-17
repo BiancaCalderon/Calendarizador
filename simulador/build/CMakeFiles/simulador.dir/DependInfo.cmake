87a96533b9c47a32bedc19902421247649c2cba4
Bianca <cal22272@uvg.edu.gt>
2025-05-16 19:09:25 -0600

    feat: examples for simulation

---
+++ simulador/build/CMakeFiles/simulador.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bianca_cal/Calendarizador/simulador/src/gui/gantt_chart_view.cpp" "CMakeFiles/simulador.dir/src/gui/gantt_chart_view.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/gui/gantt_chart_view.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/gui/main_window.cpp" "CMakeFiles/simulador.dir/src/gui/main_window.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/gui/main_window.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/gui/scheduling_view.cpp" "CMakeFiles/simulador.dir/src/gui/scheduling_view.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/gui/scheduling_view.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/gui/sync_view.cpp" "CMakeFiles/simulador.dir/src/gui/sync_view.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/gui/sync_view.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/main.cpp" "CMakeFiles/simulador.dir/src/main.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/main.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/models/action.cpp" "CMakeFiles/simulador.dir/src/models/action.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/models/action.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/models/process.cpp" "CMakeFiles/simulador.dir/src/models/process.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/models/process.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/models/resource.cpp" "CMakeFiles/simulador.dir/src/models/resource.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/models/resource.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/scheduling/fifo.cpp" "CMakeFiles/simulador.dir/src/scheduling/fifo.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/scheduling/fifo.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/scheduling/priority.cpp" "CMakeFiles/simulador.dir/src/scheduling/priority.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/scheduling/priority.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/scheduling/round_robin.cpp" "CMakeFiles/simulador.dir/src/scheduling/round_robin.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/scheduling/round_robin.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/scheduling/scheduler.cpp" "CMakeFiles/simulador.dir/src/scheduling/scheduler.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/scheduling/scheduler.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/scheduling/sjf.cpp" "CMakeFiles/simulador.dir/src/scheduling/sjf.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/scheduling/sjf.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/scheduling/srt.cpp" "CMakeFiles/simulador.dir/src/scheduling/srt.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/scheduling/srt.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/synchronization/mutex.cpp" "CMakeFiles/simulador.dir/src/synchronization/mutex.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/synchronization/mutex.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/synchronization/semaphore.cpp" "CMakeFiles/simulador.dir/src/synchronization/semaphore.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/synchronization/semaphore.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/synchronization/sync_mechanism.cpp" "CMakeFiles/simulador.dir/src/synchronization/sync_mechanism.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/synchronization/sync_mechanism.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/utils/file_loader.cpp" "CMakeFiles/simulador.dir/src/utils/file_loader.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/utils/file_loader.cpp.o.d"
+  "/home/bianca_cal/Calendarizador/simulador/src/utils/metrics.cpp" "CMakeFiles/simulador.dir/src/utils/metrics.cpp.o" "gcc" "CMakeFiles/simulador.dir/src/utils/metrics.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
